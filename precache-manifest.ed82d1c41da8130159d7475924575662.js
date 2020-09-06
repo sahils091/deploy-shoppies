2d12104aeb4367bee92f06b67f3ab00ab4ea298d
Sahil Singh <sahils091@gmail.com>
2020-09-06 19:11:25 -0400

    Updates

---
+++ precache-manifest.ed82d1c41da8130159d7475924575662.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "241e8198d4c8cba6851ebd2631c5af49",
+    "url": "/deploy-shoppies/index.html"
+  },
+  {
+    "revision": "5165ea41b227a4247266",
+    "url": "/deploy-shoppies/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0522fc99a6037e3d6a65",
+    "url": "/deploy-shoppies/static/js/2.7136017a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deploy-shoppies/static/js/2.7136017a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5165ea41b227a4247266",
+    "url": "/deploy-shoppies/static/js/main.4f096483.chunk.js"
+  },
+  {
+    "revision": "6b4e810045ccc0823876",
+    "url": "/deploy-shoppies/static/js/runtime-main.b998f8a5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deploy-shoppies/static/media/logo.5d5d9eef.svg"
+  }
+]);
